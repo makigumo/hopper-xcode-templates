200c2f323f6d911e1ef2cb3abbc0b1327d54b388
Alessandro Gatti <a.gatti@frob.it>
2017-06-05 01:50:58 +0800

    Added template for Tool plugins.

---
+++ HopperTool.xctemplate/___PACKAGENAMEASIDENTIFIER___Tool.h
@@ -0,0 +1,14 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+#import <Foundation/Foundation.h>
+#import <Hopper/Hopper.h>
+
+@interface ___PACKAGENAMEASIDENTIFIER___Tool : NSObject<HopperTool>
+
+@end
